798e411a8376913863182c713edb0c01b54ddb79
harshpurvak <196641484+harshpurvak@users.noreply.github.com>
2026-03-28 12:11:08 +0530

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,40 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      contact
-    </div>
+    <section className='section-contact'>
+
+      <h2 className='container-title'>Contact Us</h2>
+      <div className='container contact-wrapper'>
+
+      <form action="">
+        <input type="text"
+          required
+          autoComplete='false'
+          placeholder='Enter Your Name'
+          name='username'
+        />
+
+        <input type="email"
+          required
+          className='form-control'
+          placeholder='Enter Your Email'
+          name='email'
+          autoComplete='false'
+        />
+        <textarea
+          className='form-control'
+          rows="10"
+          placeholder='Enter Your Message'
+          name='message'
+          required
+          autoComplete='false'
+        >
+
+        </textarea>
+        <button type='submit'>Send</button>
+      </form>
+      </div>
+    </section>
   )
 }
 
